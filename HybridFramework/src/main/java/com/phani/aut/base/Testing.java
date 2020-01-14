0d4763c758403632f38dd4b7a6e3a2f034d892a6
phanison898 <phanison898@gmail.com>
2020-01-14 14:33:38 +0530

    updated test

--- HybridFramework/src/main/java/com/phani/aut/base/Testing.java
+++ HybridFramework/src/main/java/com/phani/aut/base/Testing.java
@@ -1,15 +1,16 @@
 package com.phani.aut.base;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Testing {
 
 	@Test
-	public void test() throws InterruptedException {
-		WebFactory.initBrowser(BrowserType.CHROME);
-		Thread.sleep(3000);
-		WebFactory.quitBrowser();
+	public void test() {
+		System.out.println("Test case started");
+		Assert.assertTrue(true);
+		System.out.println("Test case started");
 	}
 	
 }

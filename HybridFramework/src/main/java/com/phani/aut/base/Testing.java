39cb41a5cc2b282120ee77c2bbc5e1b9ab92afce
phanison898 <phanison898@gmail.com>
2020-01-14 15:43:00 +0530

    added more test cases

--- HybridFramework/src/main/java/com/phani/aut/base/Testing.java
+++ HybridFramework/src/main/java/com/phani/aut/base/Testing.java
@@ -7,10 +7,38 @@ import org.testng.annotations.Test;
 public class Testing {
 
 	@Test
-	public void test() {
-		System.out.println("Test case started");
+	public void test1() {
+		System.out.println("Test-1 case started");
 		Assert.assertTrue(true);
-		System.out.println("Test case started");
+		System.out.println("Test-1 case started");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("Test-2 case started");
+		Assert.assertTrue(true);
+		System.out.println("Test-2 case started");
+	}
+	
+	@Test
+	public void test3() {
+		System.out.println("Test-3 case started");
+		Assert.assertTrue(true);
+		System.out.println("Test-3 case started");
+	}
+	
+	@Test
+	public void test4() {
+		System.out.println("Test-4 case started");
+		Assert.assertTrue(true);
+		System.out.println("Test-4 case started");
+	}
+	
+	@Test
+	public void test5() {
+		System.out.println("Test-5 case started");
+		Assert.assertTrue(true);
+		System.out.println("Test-5 case started");
 	}
 	
 }
